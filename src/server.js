40c8a8122dad7a5c4bcf629c40b00631498e3d2f
Rafazxk <rs2789115@gmail.com>
2026-04-01 17:19:28 -0300

    fix: autocriação de tabelas no postgreSQL

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
-
+import { setupDatabase } from './config/initDb.js';
 const PORT = process.env.PORT || 10000;
+
+setupDatabase().then(() =>{ 
+  console.log("banco de dados sincronizado");
+})
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log("server rodando na porta", PORT);

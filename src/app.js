35c903f797d594f79dc837eca92a5d06f4bfa085
Rafazxk <rs2789115@gmail.com>
2026-04-01 14:02:17 -0300

    ajuste no app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,29 @@ import cors from "cors";
 
 import userRoutes from "./routes/userRoutes.js";
  import consultaRoutes from "./routes/consultaRoutes.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.json());
+console.log("Diretório atual:", __dirname);
+console.log("Tentando servir estáticos de:", path.join(__dirname, 'public'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('*', (req, res, next) => {
+    // Ignora rotas que começam com /api
+    if (req.path.startsWith('/api')) return next();
+    
+    res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
+        if (err) {
+            console.error("Erro ao enviar index.html:", err);
+            res.status(404).send("Arquivo index.html não encontrado na pasta public.");
+        }
+    });
+});
 
 app.use(cors({
   origin: "*",

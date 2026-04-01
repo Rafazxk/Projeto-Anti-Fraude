30373d97dbe7e8ff47158353d4a0c67d7badccd4
Rafazxk <rs2789115@gmail.com>
2026-04-01 14:59:42 -0300

    ajuste no SSL para cadastro de usuarios

--- src/config/database.js
+++ src/config/database.js
@@ -1,33 +1,31 @@
 import pkg from "pg";
 import dotenv from "dotenv";
 
-//startar
-//pg_ctl -D $PREFIX/var/lib/postgresql start
-// psql -d guardix
-
 dotenv.config();
 
 const { Pool } = pkg;
 
 if (!process.env.DATABASE_URL) {
-  console.error("DATABASE_URL não encontrada no .env");
+  console.error("DATABASE_URL não encontrada nas variáveis de ambiente");
   process.exit(1);
 }
 
-// console.log("database url: ", process.env.DATABASE_URL);
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-
+  connectionString: process.env.DATABASE_URL,
+  // ADICIONE ISSO AQUI:
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
-try{
-const client = await pool.connect();
-console.log("conectou no banco")
-
-
-client.release();  
-} catch(err){
-  console.error(err);
+// Teste de conexão com tratamento de erro melhorado
+try {
+  const client = await pool.connect();
+  console.log("--- [BANCO] Conexão estabelecida com sucesso no Render! ---");
+  client.release();  
+} catch (err) {
+  console.error("--- [ERRO BANCO] Falha ao conectar: ---");
+  console.error(err.message);
 }
+
 export default pool;
